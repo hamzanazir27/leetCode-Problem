24f76bcfeb515ae9768ce69ebb935d5a471a7984
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-07-26 13:26:36 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+     if (lists.size()==1)
+        return lists[0];
+     if (lists.empty())
+          return nullptr;
+
+
+
+
+       int left=0;
+      int right=0;
+       for(right=1;right<lists.size(); right++)
+       {
+        lists[right]=handleSort(lists[left],lists[right]);
+       }
+
+
+       return  lists[left];
+       
+
+
+
+       
+    }
+
+     ListNode* handleSort(ListNode* list1,ListNode* list2)
+     {
+            if (!list1) {
+            return list2;
+        }
+        if (!list2) {
+            return list1;
+        }
+
+        ListNode* head = new ListNode();
+        ListNode* cur = head;
+        while (list1 &&  list2) {
+            if (list1->val > list2->val) {
+                cur->next = list2;
+                cur = list2;
+                list2 = list2->next;
+            } else {
+                cur->next = list1;
+                cur = list1;
+                list1 = list1->next;
+            }
+        }
+
+        if(!list1)cur->next=list2;
+        else  cur->next=list1;
+
+        cur =head;
+        head=cur->next;
+        delete cur;
+        return head;
+    }//handleSort
+
+    
+     
+};
