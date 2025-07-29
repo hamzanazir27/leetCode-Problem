d048b257ccd5d27c2b50f8549775da0bc40e0ffd
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-07-29 09:08:45 +0500

    Time: 1 ms (60.08%), Space: 8.7 MB (41.31%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,24 +1,51 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        //check negtive number 
-    if(0>x || x%10==0 &&x !=0 )
-    return false;
-   //171
+        if (x < 0 || (x % 10 == 0 && x != 0)) 
+            return false;
 
-    int reversedHalf=0;  
-    while(reversedHalf<x)
-    {
-        int lastdigit=  x%10;    //171       171/10    
-        //adding to reverse
-        reversedHalf=reversedHalf*10+lastdigit;
-        x=x/10;     
-
+        return x == reverse(x);
     }
 
+private:
+    long long reverse(int remainingleft, long long right = 0)
+    {
+        if (remainingleft == 0) 
+            return right;
 
+        int ri = remainingleft % 10;
+        int left = remainingleft / 10;
 
-    return x==reversedHalf  || x ==reversedHalf/10;
-
+        return reverse(left, right * 10 + ri);
     }
 };
+
+
+
+
+// approch without recursion
+
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         //check negtive number 
+//     if(0>x || x%10==0 &&x !=0 )
+//     return false;
+//    //171
+
+//     int reversedHalf=0;  
+//     while(reversedHalf<x)
+//     {
+//         int lastdigit=  x%10;    //171       171/10    
+//         //adding to reverse
+//         reversedHalf=reversedHalf*10+lastdigit;
+//         x=x/10;     
+
+//     }
+
+
+
+//     return x==reversedHalf  || x ==reversedHalf/10;
+
+//     }
+// };
