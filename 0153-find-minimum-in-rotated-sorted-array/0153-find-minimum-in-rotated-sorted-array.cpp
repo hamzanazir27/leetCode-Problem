1ac8acb87d7eec9a433eaf44b7ab1a3f11a3831f
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-08-11 21:33:59 +0500

    Time: 0 ms (100%), Space: 14.3 MB (12.38%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,25 +1,104 @@
+/*
+```
+//my approch is
+
+ -> minValue = is me Maxrakh do;
+ left =0,right=lastindex;
+
+ while(left<=right)
+{
+
+  //update minValue:  campare with current mid value of array
+
+  minVal=minimum ( arr[mid],minValue);
 
 
+//check left side is sorrted or not
+if(arr[mid]>arr[left])// if sorted
+  {
+
+
+       // ager tu sorted hoi tu is ka matalab hai jo left index hai
+       //vo sub se minum value hai aur right side pe may be is se b chota ho tu
+hum kya krain gey minvalue update krain gey campare with minvaue and arr[left]
+aur move kr jaye gey right side pe
+
+// // // // // // // // // // //
+  }
+  else//ager left sorted nhi tu right sorted hai aur ye mid index saub se choti
+value hai
+  {
+    //ab hum hume pta hai right side tu bari value hi miley tu left sid emove
+kraiun gey right =mid-1
+  }
+
+}
+
+
+return minvalue
+
+
+```
+--------------------------------------------------------------
+
+```
+
+
+Template
+min = max
+
+while (left <= right)
+{
+  if(min>arr[mid])
+
+    store in min
+
+  //move left or right
+
+  if(arr[mid]>arr[left])//sorted left
+    {
+      min =min(arr[left], min)
+      //move left to right
+      left=mid+1;
+
+    }
+    else
+    {
+      move right to left
+
+      left =left+1;
+
+
+
+    }
+}
+
+return minVal;
+```
+------------------------------------------------------------------
+
+
+*/
 
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int mini = INT_MAX ;
-        int low = 0 ;
-        int high =  nums.size() - 1;
-        while(low <= high){
-            int mid = low + (high - low)/2 ;
+        int left = 0;
+        int right = nums.size() - 1;
+        int minVal = INT_MAX;
+        while (left <= right) {
+            int mid = right + left / 2;
 
-            if(nums[mid] < mini)
-                mini = nums[mid] ;
-            
-            if(nums[low] <= nums[mid]){
-                mini = min(nums[low], mini) ;
-                low = mid + 1;
+            minVal = min(nums[mid], minVal);
+
+            if (nums[mid] > nums[left]) {
+                minVal = min(nums[left], minVal);
+                // move right side
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            else
-                high = mid - 1 ;
         }
-        return mini; 
+        return minVal;
     }
 };
