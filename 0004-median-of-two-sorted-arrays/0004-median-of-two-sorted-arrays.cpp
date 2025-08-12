bdb90ee882ccc640b2d7dba512cfb1e005947d10
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-08-12 11:54:56 +0500

    Time: 7 ms (10.48%), Space: 96 MB (10.37%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,4 +1,53 @@
-/*
+
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         // Ensure nums1 is smaller
+//         if (nums1.size() > nums2.size())
+//             return findMedianSortedArrays(nums2, nums1);
+
+//         int n1 = nums1.size();
+//         int n2 = nums2.size();
+//         int low = 0, high = n1;
+//         int totalLeft = (n1 + n2 + 1) / 2;
+
+//         while (low <= high) {
+//             int partitionNums1 = (low + high) / 2;
+//             int partitionNums2 = totalLeft - partitionNums1;
+
+//             int maxLeftNums1 = (partitionNums1 == 0) ? INT_MIN : nums1[partitionNums1 - 1];
+//             int minRightNums1 = (partitionNums1 == n1) ? INT_MAX : nums1[partitionNums1];
+
+//             int maxLeftNums2 = (partitionNums2 == 0) ? INT_MIN : nums2[partitionNums2 - 1];
+//             int minRightNums2 = (partitionNums2 == n2) ? INT_MAX : nums2[partitionNums2];
+
+//             if (maxLeftNums1 <= minRightNums2 && maxLeftNums2 <= minRightNums1) {
+//                 if ((n1 + n2) % 2 == 1) {
+//                     return max(maxLeftNums1, maxLeftNums2);
+//                 } else {
+//                     return (max(maxLeftNums1, maxLeftNums2) + min(minRightNums1, minRightNums2)) / 2.0;
+//                 }
+//             } else if (maxLeftNums1 > minRightNums2) {
+//                 high = partitionNums1 - 1;
+//             } else {
+//                 low = partitionNums1 + 1;
+//             }
+//         }
+//         return 0.0; // Should never reach here
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+/*  MY APPROCH 
 Understanding:
 --------------
 We are given two sorted arrays, for example:
@@ -87,3 +136,5 @@ public:
         return median;
     }
 };
+
+
