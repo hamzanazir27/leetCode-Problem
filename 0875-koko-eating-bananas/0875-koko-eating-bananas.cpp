457f7f625d34b5cf4f0d4110f6413a503b8dd7c1
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-08-14 16:02:47 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,85 +3,63 @@
 // Koko has several piles of bananas. She has h hours before the guards return.
 // She will choose a fixed eating speed k (bananas per hour).
 // In each hour, she will choose one pile and eat exactly k bananas from it.
-// If the pile has more than k bananas → she eats k and leaves the rest for later.
-// If the pile has less than k bananas → she eats all of them and doesn’t eat
-// anything else in that hour. The goal is to find the smallest possible k that
-// allows Koko to finish all bananas in h hours or less. If k is too small → she
-// will be too slow and run out of time. If k is big enough → she will finish
-// earlier, but we want the slowest speed that still finishes on time.
+// If the pile has more than k bananas → she eats k and leaves the rest for
+// later. If the pile has less than k bananas → she eats all of them and doesn’t
+// eat anything else in that hour. The goal is to find the smallest possible k
+// that allows Koko to finish all bananas in h hours or less. If k is too small
+// → she will be too slow and run out of time. If k is big enough → she will
+// finish earlier, but we want the slowest speed that still finishes on time.
 
+// Approch
 
+// max number in array
+// minimum 1 kha sakta hai koko banana
+// ab maximum lgain tu hours k equal b ho sakta hai aur km b aur minimum
+// lgayauin tu array k size k equal b aur km b tu hum ne ya tu min ya max ya in
+// k drmayan wala koi number lena hai
 
-
-
-// */
-
-// class Solution {
-// public:
-//     int minEatingSpeed(vector<int>& piles, int h) {
-
-//         int left = 1;
-//         int mid = 0;
-//         int right = INT_MIN;
-//         for (int pil : piles)
-//             right = max(pil, right);
-
-//         while (left <= right) {
-//             mid = left + (right - left) / 2;
-//             if (h == helperCheck(piles, mid)) {
-//                 return mid;
-//             } else if (mid > h) {
-//                 right = mid - 1;
-//             } else {
-//                 left = mid + 1;
-//             }
-
-//         } // while ---->|||||||||||||||||||||||||||||||| ------->
-
-//         return mid;
-//     }
-
-//     int helperCheck(vector<int>& piles, int h) {
-//         int num = 0;
-//         for (int pil : piles)
-//             num = num + ceil(pil / h);
-
-//         return num;
-//     }
-// };
-
-
+// tu hum binary search lgai mid ko check kiya hourn  k equal aur km acceptble
+// hai koshesh kro equal mil jaye
+//   nhi tu km tu hum ne condition lgai min<=max
+// mid ko check krtey tehy ager tu mid k hisab se km value aye tu hour se km
+// value aye tu hamey ye axceptble hai but hum ne kaha ager tu is se br jaye
+// slice tu best hai slice tb hi barey ga ager min jb slice chota kiya jaye
+// metlab maximum mid k bad rakh do else ager slice hour se bsr jaye tu slice ko
+// bara kro left mid k bad rakho
 
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1;
-        int right = *max_element(piles.begin(), piles.end());
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            long long totalHours = helperCheck(piles, mid);
+        int minV = 1;
+        int mid = 0;
+        int maxV = INT_MIN;
+        for (int pil : piles)
+            maxV = max(pil, maxV);
 
-            if (totalHours <= h) {
+        while (minV <= maxV) {
+            mid = minV + (maxV - minV) / 2;
 
-                // Speed is enough, try slower
-                right = mid - 1;
-
-            } else {
-
-                // Speed is too slow
-                left = mid + 1;
-
+            if (h == helperCheck(piles, mid)) {
+                return mid;
             }
-        }
-        return left; // smallest valid speed
+
+            else if (h >= helperCheck(piles, mid)) {
+                maxV = mid - 1;
+            } else {
+                minV = mid + 1;
+            }
+
+        } // while ---->|||||||||||||||||||||||||||||||| ------->
+
+        return minV;
     }
 
-    long long  helperCheck(vector<int>& piles, int speed) {
-        long long  total = 0;
-        for (int pil : piles) {
-            total += ceil((double)pil / speed);
-        }
-        return total;
+    long long  helperCheck(vector<int>& piles, int h) {
+        long long  num = 0;
+        for (int pil : piles)
+            num = num + ceil((double)pil / (double)h);
+
+        return num;
     }
 };
