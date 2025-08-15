7131e3c950a61210ee47bca94204165219d6ea17
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-08-15 12:08:05 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<int> stk;
+        vector<int> ans(n, 0);
+        for (int i = (temperatures.size() - 1); 0 <= i; i--) {
+
+            if (stk.empty()) {
+                // ans[i] = stk.top() - i;
+                stk.push(i);
+            } else if (temperatures[i] >= temperatures[stk.top()]) {
+
+                while (!stk.empty() && temperatures[i] > temperatures[stk.top()] )
+                       {
+                    stk.pop();
+                }
+                if(!stk.empty())
+                ans[i] = stk.top() - i;
+                stk.push(i);
+
+            } else {
+                ans[i] = stk.top() - i;
+                stk.push(i);
+            }
+        } // for
+        return ans;
+    }
+};
