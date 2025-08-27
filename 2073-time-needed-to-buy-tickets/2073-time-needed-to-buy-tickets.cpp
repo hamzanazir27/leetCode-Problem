fac54877172af08bf480f3192c8bb43ae95139b9
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-08-27 12:36:41 +0500

    Time: 3 ms (20.49%), Space: 12.7 MB (8%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,3 +1,46 @@
+
+// using queue
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < tickets.size(); i++) {
+            q.push({i, tickets[i]});
+        }
+
+        int time = 0;
+
+        while (!q.empty()) {
+            auto [pos, remain] = q.front();
+            q.pop();
+            remain--;
+            time++;
+            if (remain > 0) {
+                q.push({pos, remain});
+            }
+            if(pos==k&& remain ==0)
+            {
+                return time;
+            }
+        }
+        return time;
+    }
+};
+
+// class Solution {
+// public:
+//     int timeRequiredToBuy(vector<int>& tickets, int k) {
+
+//     }
+// };
+
+/*
+Simulation approch
+------------------
+
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
@@ -5,9 +48,8 @@ public:
         int i = 0;
 
         while (tickets[k] != 0) {
-            if (tickets[i] > 0) {   // sirf unhi logon ko ticket do jinke pass bachi ho
-                tickets[i]--;
-                sec++;
+            if (tickets[i] > 0) {   // sirf unhi logon ko ticket do jinke pass
+bachi ho tickets[i]--; sec++;
             }
             i++;
             if (i == tickets.size()) i = 0; // full round complete ho gaya
@@ -15,3 +57,18 @@ public:
         return sec;
     }
 };
+
+
+
+Space Complexity: O(1)
+Kyunki hum sirf kuch variables (sec, i) use kar rahe hain, extra space nahi le
+rahe. Tickets array ko modify kar rahe hain, lekin extra memory nahi ban rahi.
+Time Complexity: O(n * m)
+n = tickets.size() (number of people)
+m = tickets[k] (number of tickets kth person wants)
+Worst case: loop har person ke liye chal sakta hai har ticket khatam hone tak →
+total ≈ n * m operations
+
+
+
+*/
