b0d21b8b57a0ba98792c15a40523fddd375db8f8
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-09-27 10:11:23 +0500

    Time: 3 ms (66.58%), Space: 8.4 MB (11.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,27 +1,28 @@
+
+//learning
+
 class Solution {
 public:
     int fib(int n) {
-        // return  fibN(n);
-        
-        //base case 
-        if(n==1)
-          return 1;
-         if(n==0)
-          return 0;
-
-        return fib(n-1)+fib(n-2);
+    vector<int> dp(n+1,-1);
+       return helper(n,dp);
     }
 
+    int helper(int n,vector<int> &dp)
+    {
+          if (n <= 1) return n;
 
-// private:
-//     int fibN(int a)
-//     {
-//         //base case 
-//         if(a==1)
-//           return 1;
-//          if(a==0)
-//           return 0;
 
-//         return fibN(a-1)+fibN(a-2);
-//     }
+          
+
+        if(dp[n]!=-1)
+        {
+            return  dp[n]; 
+        }
+
+
+        dp[n]=helper(n-1,dp)+helper(n-2,dp);
+        return dp[n];
+
+    }
 };
