6f806612e17875910d2b5b9e3d596e6465e44d92
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-09-28 09:50:08 +0500

    Time: 0 ms (100%), Space: 8.3 MB (10.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,28 +1,79 @@
 
-//learning
+//recursion
+class Solution3 {
+public:
+    int fib(int n) {
+        if(n<=1)
+           return n;
+        
+        return fib(n-1)+fib(n-2);
+    }
+};
 
+//dp topdown approch /. Memoization Approach ✅
+
+class Solution2 {
+public:
+    int fib(int n) {
+     vector<int> dp(n+1,-1);
+     return helper(n,dp);
+    }
+
+    int helper(int n, vector<int> &dp)
+    {
+        if(n<=1)
+           return n;
+
+        if(dp[n] !=-1)
+          return dp[n];
+        
+        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+    }
+};
+
+
+
+//2. Tabulation Approach 
+class Solution1 {
+public:
+    int fib(int n) {
+     vector<int> dp(n+1,-1);
+
+        if(n<=1)
+           return n;
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+    }
+};
+
+
+
+//3. Space Optimized Approach 
 class Solution {
 public:
     int fib(int n) {
-    vector<int> dp(n+1,-1);
-       return helper(n,dp);
-    }
+     vector<int> dp(n+1,-1);
 
-    int helper(int n,vector<int> &dp)
-    {
-          if (n <= 1) return n;
-
-
-          
-
-        if(dp[n]!=-1)
+        if(n<=1)
+           return n;
+        int prev1=1;
+        int prev2=0;
+        for(int i=2;i<=n;i++)
         {
-            return  dp[n]; 
+            int cur=prev1+prev2;
+            prev2=prev1;
+            prev1=cur;
         }
-
-
-        dp[n]=helper(n-1,dp)+helper(n-2,dp);
-        return dp[n];
-
+        
+    return prev1;
     }
 };
+
+
