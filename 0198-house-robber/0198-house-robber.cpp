1c93aae6a9532f6090fe8c6d173198fff9d889d2
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-10-01 08:45:57 +0500

    Time: 0 ms (100%), Space: 10.6 MB (46.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -26,7 +26,7 @@ if(index==1) return max(nums[0], nums[1]);
 
 //1st dp memorization approch of db (topdown apptoch)
 
-class Solution {
+class Solution2 {
 public:
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size(),-1);
@@ -37,7 +37,7 @@ public:
     int helper(int index,vector<int>& nums,vector<int> &dp)
     {
   if(index==0) return nums[0];
-if(index==1) return max(nums[0], nums[1]);  
+    if(index==1) return max(nums[0], nums[1]);  
 
         if(dp[index]!=-1)
         {
@@ -51,6 +51,32 @@ if(index==1) return max(nums[0], nums[1]);
         return dp[index]=max(skip , rob);
                    
     }
+};
+
+//2nd  dp tabular approch of db (bottom up apptoch)
+
+class Solution{
+public:
+    int rob(vector<int>& nums) 
+    {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp(n);
+
+    dp[0]= nums[0];
+    dp[1]= max(nums[0], nums[1]); 
+             
+     for (int i=2;i<nums.size();i++)
+     {
+        dp[i]=max((nums[i]+dp[i-2]),dp[i-1]);
+     }
+
+
+
+     return dp[nums.size()-1];
+        
+    }
+
 };
 
 
