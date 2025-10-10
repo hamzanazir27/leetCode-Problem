b161aad435032f41a3a607f8c515a3422ae033c0
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-10-10 12:11:34 +0500

    Time: 0 ms (100%), Space: 17.7 MB (54.09%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,19 +1,49 @@
+class SolutionRecursion {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        return min(helper(cost, 0), helper(cost, 1));
+    }
+
+    int helper(vector<int>& cost, int index) {
+        if (index >= cost.size())
+            return 0;
+
+        return min(cost[index] + helper(cost, index + 1),
+                   cost[index] + helper(cost, index + 2));
+    }
+};
+
+// approch 2 dp memorization
+class SolutionMemorization {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size(), -1);
+
+        return min(helper(cost, 0, dp), helper(cost, 1, dp));
+    }
+
+    int helper(vector<int>& cost, int index, vector<int>& dp) {
+        if (index >= cost.size())
+            return 0;
+        if (dp[index] != -1)
+            return dp[index];
+
+        return dp[index] = min(cost[index] + helper(cost, index + 1, dp),
+                               cost[index] + helper(cost, index + 2, dp));
+    }
+};
+// approch 3 dp Tabulation approch
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-                vector<int> dp(cost.size(), -1);
+        vector<int> dp(cost.size() + 1, -1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        ///////////////////////////
+        for (int i = 2; i < cost.size(); i++) {
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+        }
 
-        return min(helper(cost,0,dp),helper(cost,1,dp));
-
+        return min(dp[cost.size()-1] ,dp[cost.size()-2]) ;
     }
-
-    int helper(vector<int>& cost,int index,vector<int>& dp) {
-        if(index>=cost.size())
-            return 0;
-        if(dp[index] !=-1)
-           return dp[index];
-
-       return dp[index]=min(cost[index]+helper(cost,index+1,dp),cost[index]+helper(cost,index+2,dp));
-    }
-
 };
