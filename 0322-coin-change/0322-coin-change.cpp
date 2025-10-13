2c1f1e8bd1eed11699b04b8b2a0397e0be30d43e
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-10-13 12:22:17 +0500

    Time: 15 ms (92.9%), Space: 18 MB (62.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -54,20 +54,28 @@ public:
         return dp[amount] = mini;
     }
 };
+
 // DP_Tabulation
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount + 1, INT_MAX);
-        dp[0] = 0; // Base case: 0 amount = 0 coins
-
-        for (int i = 0; i < coins.size(); i++) {
+        dp[0] = 0;
+        for (int i = 0; i < coins.size(); i++)
+        {
             for (int j = coins[i]; j <= amount; j++) {
-                if (dp[j - coins[i]] != INT_MAX)
-                    dp[j] = min(dp[j], 1 + dp[j - coins[i]]);
-            }
-        }
+                if (dp[j - coins[i]] != INT_MAX) {
 
-        return dp[amount] != INT_MAX ? dp[amount] : -1;
+
+                  dp[j]=min(dp[j],1+dp[j - coins[i]]);   
+
+                }//if
+
+            } // for
+
+        }//for
+
+
+        return dp[amount]!=INT_MAX ? dp[amount] : -1;
     }
 };
