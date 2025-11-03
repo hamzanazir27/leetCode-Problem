00e45a4bb92c300471bab446682e448e5a80c19d
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-11-03 22:57:43 +0500

    Time: 13 ms (13.19%), Space: 20.6 MB (16.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,44 +1,46 @@
-#include <vector>
-#include <unordered_map>
-#include <queue>
-using namespace std;
+// #include <vector>
+// #include <unordered_map>
+// #include <queue>
+// using namespace std;
 
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, int> indegree;
+
         unordered_map<int, vector<int>> graph;
-
-        // Initialize indegree for all courses
-        for (int i = 0; i < numCourses; i++)
-            indegree[i] = 0;
-
-        // Build graph and indegree count
-        for (auto& pre : prerequisites) {
-            int course = pre[0];
-            int prereq = pre[1];
-            graph[prereq].push_back(course);
-            indegree[course]++;
-        }
-
-        // Topological Sort (Kahn's Algorithm)
+        unordered_map<int, int> count;
         queue<int> q;
-        for (auto& p : indegree)
-            if (p.second == 0) q.push(p.first);
+        for (auto& ele : prerequisites) {
+            int a = ele[0];
+            int b = ele[1];
 
-        int count = 0;
-        while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            count++;
-
-            for (int next : graph[curr]) {
-                indegree[next]--;
-                if (indegree[next] == 0)
-                    q.push(next);
+            graph[b].push_back(a);
+            count[a]++;
+            if (count.find(b) == count.end()) {
+                count[b] = 0;
             }
         }
 
-        return count == numCourses;
+        for (int i = 0; i < numCourses; i++) {
+            if (count[i] == 0)
+                q.push(i);
+        }
+
+        int countCourses = 0;
+        while (!q.empty()) {
+            countCourses++;
+            int a = q.front();
+            q.pop();
+
+            for (auto& neibCourse : graph[a])
+                if (count.find(neibCourse) != count.end()) {
+                    count[neibCourse]--;
+                    if (count[neibCourse] == 0) {
+                        q.push(neibCourse);
+                    }
+                }
+        }
+
+        return countCourses == numCourses;
     }
 };
