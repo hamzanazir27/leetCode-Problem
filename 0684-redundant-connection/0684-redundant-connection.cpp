20a80234ef147670babe930320afa5d908c9b6c6
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-11-09 12:07:01 +0500

    Time: 0 ms (100%), Space: 13 MB (66.33%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        
-        // Parent array - initially har node apna parent
-        vector<int> parent(n + 1);
-        for(int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-        
-        // Process each edge
-        for(auto& edge : edges) {
-            int node1 = edge[0];
-            int node2 = edge[1];
-            
-            // Find roots of both nodes
-            int root1 = findRoot(parent, node1);
-            int root2 = findRoot(parent, node2);
-            
-            // Agar same root hai, matlab cycle mil gaya!
-            if(root1 == root2) {
-                return edge;  // Ye edge redundant hai
-            }
-            
-            // Otherwise, union kar do (connect them)
-            parent[root1] = root2;
-        }
-        
-        return {};  // Should never reach here
+     int N=edges.size();
+
+    vector<int> parent(N+1);
+    for(int i=0;i<=N;i++)
+    {
+        parent[i]=i;
     }
     
-private:
-    int findRoot(vector<int>& parent, int node) {
-        // Path compression ke saath root find karo
-        if(parent[node] != node) {
-            parent[node] = findRoot(parent, parent[node]);
+    for(auto& edge:edges)
+    {
+        int node1=edge[0];
+        int node2=edge[1];
+         
+        int root1=findRoot(parent,node1);
+        int root2=findRoot(parent,node2);
+
+        if(root1 == root2)
+        {
+            return edge;
+        }
+        parent[root1]=root2;
+        
+    }//for
+
+     return {};
+    }
+    //findRedundantConnection
+
+
+    int findRoot(vector<int>& parent,int node)
+    {
+        if(parent[node]!=node)
+        {
+            parent[node]=findRoot( parent, parent[node]);
         }
         return parent[node];
     }
