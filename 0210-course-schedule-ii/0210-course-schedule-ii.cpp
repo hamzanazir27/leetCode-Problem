ede770587faabb14f8b332e119adefca892cfb3b
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2025-11-25 09:58:19 +0500

    Time: 3 ms (73.11%), Space: 18.5 MB (55.1%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,48 +1,123 @@
 class Solution {
-public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> graph;
-        unordered_map<int, int> count;
-        queue<int> q;
-        for (auto& ele : prerequisites) {
-            int a = ele[0];
-            int b = ele[1];
+public: 
+    bool isCylicDFS(int src,vector<vector<int>>& adjList,vector<bool>& visit,vector<bool>& isRecursive)
+    {
+         isRecursive[src]=true;
+         visit[src]=true;
 
-            graph[b].push_back(a);
-            count[a]++;
-            if (count.find(b) == count.end()) {
-                count[b] = 0;
-            }
-        }
-
-        for (int i = 0; i < numCourses; i++) {
-            if (count[i] == 0)
-                q.push(i);
-       
-     }
-        
-      
-
-        vector<int> result;
-        while (!q.empty()) {
-            
-            int a = q.front();
-            q.pop();
-            result.push_back(a);
-
-            for (auto& neibCourse : graph[a])
-                if (count.find(neibCourse) != count.end()) {
-                    count[neibCourse]--;
-                    if (count[neibCourse] == 0) {
-                        q.push(neibCourse);
-                    }
+        for(auto naib:adjList[src])
+        {
+                if(!visit[naib])
+                {
+                   if(isCylicDFS(naib,adjList,visit,isRecursive))
+                   {
+                      return true;
+                   }
+                }
+                else if(isRecursive[naib])
+                {
+                      return true;
                 }
         }
 
-        if (result.size() != numCourses)
-            return {};
+         
+        
+      isRecursive[src]=false;
+      return false;
+
+
+    }
+
+
+public: 
+    void topoLog(int src,vector<vector<int>>& adjList,vector<bool>& visit,stack<int>& s)
+    {
+         visit[src]=true;
+
+        for(auto naib:adjList[src])
+        {
+                if(!visit[naib])
+                {
+                   topoLog(naib,adjList,visit,s);
+                }
+             
+        }
+
+         
+        
+      
+      s.push(src);
+
+
+    }
+
+
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+
+        vector<vector<int>> adjList(n);
+        vector<bool> visit(n, false);
+        vector<bool> isRecursive(n, false);
+
+        for(auto& edge:edges)
+        {
+            //u -> v
+            // u[1] -> v[0]
+            int u= edge[1];
+            int v= edge[0];
+            adjList[u].push_back(v);
+
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(!visit[i])
+              {
+                if(isCylicDFS(i,adjList,visit,isRecursive))
+                {
+                    return {};
+                }
+              }
+        } 
+
+
+        stack<int> s;
+        vector<bool> visit2(n, false);
+         for(int i=0;i<n;i++)
+        {
+            if(!visit2[i])
+              {
+                topoLog(i,adjList,visit2,s);
+         
+              }
+        } 
+
+         vector<int> result;
+        for(int i=0;i<n;i++)
+        {
+            int a =s.top();
+            s.pop();
+            result.push_back(a);
+
+        } 
+
+
 
         return result;
 
+
+
+
+
+
+
+
+       
+
+         
+        
+
+
+        
     }
 };
