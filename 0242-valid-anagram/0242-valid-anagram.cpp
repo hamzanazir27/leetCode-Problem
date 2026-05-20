a4033d3a7f975dbf73bb1e8dd85300ac7e473f38
Hamza nazir <138714431+hamzanazir27@users.noreply.github.com>
2026-05-20 12:39:03 +0500

    Time: 0 ms (100%), Space: 10 MB (10.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+         return false;
+
+        unordered_map<char,int> s_count,t_count;
+        for(int i=0;i<s.size();i++)
+        {
+            s_count[s[i]]++;
+            t_count[t[i]]++;
+        }
+        return s_count==t_count;
+    }
+};
